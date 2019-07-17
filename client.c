aeae378eed8c83cd0c4c4c8d005f957568e45525
Zach Grimes <47222513+zfgrimez@users.noreply.github.com>
2019-07-16 23:08:16 -0500

    Add files via upload

---
+++ client.c
@@ -0,0 +1,152 @@
+/* Assignment 2
+ Author: Zach Grimes
+ PSID: 1373277
+ Dr. Rincon T/Thu Spring 2019
+ **Note that lines 87 to 102 was written by Robert Ingall and posted by Dr. Rincon on BB (used with permission.)
+ NOTE: TO COMPILE ON THE CS SERVER YOU MUST SPECIFY "gcc -std=gnu99 ./zach_grimes_client.c -lm -o client; ./client localhost portnum < somefile.txt"
+ -lm allows for the pow() function to be used by the gnu compiler, which I use for base conv. gnu99 is necessary to run the sample server code, or you'll get an h_addr not defined error.
+ */
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdbool.h>
+#include <math.h>
+void error(char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+void decode(int* msg, char pnum) {
+	int dsig[12];
+	int bbit[3]={0,0,0};
+	int val=0;
+	int j=2;
+	//decode msg
+	for(int i=0; i < 3; i++)
+		for(int j=(i*4); j < ((i+1)*4);j++)
+			dsig[j]=msg[j]*msg[(j+12)-(i*4)];
+
+	for(int i=0; i < 3; i++) {
+		for(int j = (i*4); j < ((i+1)*4); j++)
+			bbit[i] += dsig[j];
+		bbit[i]/=4;
+	}
+	for(int i=0; i<3;i++) {
+		if(bbit[i]==1)
+			val+=(int)(pow(2.0,(double)(j)));
+		j--;
+	}
+	//Format output
+	printf("\nChild %c\n",pnum);
+	printf("Signal: ");
+	for(int i=0; i < 12; i++)
+		printf("%i ", msg[i]);
+	printf("\nCode: ");
+	for(int i=12; i < 16; i++)
+		printf("%i ", msg[i]);
+	printf("\nReceived value = %i\n", val);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;
+	pid_t pid;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+	char buffer[3], fileInputBuffer[9];
+	int ReadBuffer[17];
+	size_t current_size = 0;
+	size_t size=5;
+	
+    if (argc < 3) {
+	   fprintf(stderr,"Usage: %s hostname port < inputfile.txt\n", argv[0]);
+       exit(0);
+    }
+    portno = atoi(argv[2]);
+	
+	/* <--- Read file input into a input file buffer ---> */
+	char* inputline;
+	inputline = (char *)malloc(size * sizeof(char));
+	for(int i=0;i<3;i++){
+		getline(&inputline,&size,stdin);
+		fileInputBuffer[current_size]=((i+1)+'0');
+		fileInputBuffer[(current_size+1)]=inputline[0];
+		fileInputBuffer[(current_size+2)]=inputline[2];
+		current_size+=3;
+	}
+	/* <--- Create and manage child processes ---> */
+	for(int i=0;i<3;i++){
+		if((pid=fork())==0) {
+	/* <--- Begin process of establishing connection with encoder server ---> */
+			sockfd = socket(AF_INET, SOCK_STREAM, 0);
+			if (sockfd < 0)
+				error("TCP CLIENT ERROR: opening socket failed (socket in use by another process)");
+			server = gethostbyname(argv[1]);
+			if (server == NULL) {
+				fprintf(stderr,"TCP CLIENT ERROR: no such host found\n");
+				exit(0);
+			}
+			bzero((char *) &serv_addr, sizeof(serv_addr));
+			serv_addr.sin_family = AF_INET;
+			bcopy((char *)server->h_addr,
+				  (char *)&serv_addr.sin_addr.s_addr,
+				  server->h_length);
+			serv_addr.sin_port = htons(portno);
+			if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
+				error("TCP CLIENT ERROR: failed to establish connection");
+			
+			bzero(buffer,3);
+			if(i==0) { //load write buffer
+				buffer[0]=fileInputBuffer[0];
+				buffer[1]=fileInputBuffer[1];
+				buffer[2]=fileInputBuffer[2];
+			}
+			else if(i==1) {
+				buffer[0]=fileInputBuffer[3];
+				buffer[1]=fileInputBuffer[4];
+				buffer[2]=fileInputBuffer[5];
+			}
+			else {
+				buffer[0]=fileInputBuffer[6];
+				buffer[1]=fileInputBuffer[7];
+				buffer[2]=fileInputBuffer[8];
+			}
+			printf("Child %c, sending value %c: to child process %c\n", buffer[0], buffer[2], buffer[1]);
+			n = write(sockfd,buffer,strlen(buffer));
+			/*
+			 buffer format (char array) = [ch. process sending data(int 1-3), ch. process to receive information(int 1-3), code to send(int 0-7)]
+			 */
+			if (n < 0) {
+				error("TCP CLIENT ERROR: writing to socket failed");
+				_exit(1);
+			}
+			//prep, read, and translate buffer
+			bzero(ReadBuffer, 17);
+			n = read(sockfd, ReadBuffer, sizeof(int)*16);
+			for(int i = 0; i < 16; i++)
+				ReadBuffer[i] = ntohl(ReadBuffer[i]);
+			if (n == -1) {
+				error("TCP CLIENT ERROR: reading from socket failed");
+				_exit(1);
+			}
+			else {
+				decode(ReadBuffer, buffer[0]);
+				_exit(1);
+			}
+		}
+		sleep(1);
+	}
+	//call parent to loop three times
+	for (int i=0;i<3;i++)
+		wait(NULL);
+	
+    return 0;
+}
+
---
+++ server_encryptor.c
@@ -0,0 +1,145 @@
+/*
+ Assignment 2 Program
+ Author: Zach Grimes
+ PSID: 1373277
+ Dr. Rincon- T/THU Spring 2019
+ **References: This program uses Robert Ingall's sample code posted by Dr. Rincon on BB to define the server (lines 71-81) (used with permission.)
+ NOTE: TO COMPILE ON THE CS SERVER YOU MUST SPECIFY "gcc -std=gnu99 ./zach_grimes_server.c -lm -o server; ./server localhost portnum < somefile.txt"
+ -lm allows for the pow() function to be used by the gnu compiler, which I use for base conv. gnu99 is necessary to run the sample server code, or you'll get an h_addr not defined error.
+*/
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
+void error(char *msg)
+{
+	perror(msg);
+	exit(1);
+}
+
+/* Converts a single digit base 8 number into a bipolar base 2 number */
+int* BipolBinaryConv(int val) {
+	int* bbits = (int*) malloc(sizeof(int) * 3);
+	int quot=val;
+	for(int i=2; i>=0; i--) {
+		if( (quot - (2*(quot/2))) )
+			bbits[i] = 1;
+		else
+			bbits[i] = -1;
+		quot/=2;
+	}
+	return bbits;
+}
+/*
+ Call to encoder three times, each time passing a base 8 number-
+ and it's corresponding Walsh code, converting it to bipolar binary.
+ 
+ Each EM[p] value is calculated as follows:
+ EM[1...p-1] = Bipolar Binary digit[0...3] * w[1...p-1]
+ where p is the size of the Hadamard matrix
+ 
+ Final EM is summed in the main function due to scope of assignment.
+ */
+int* Encoder(int* code, int val)
+{
+	int* signal = (int*) malloc(sizeof(int) * 12);
+	int* bbinVal = BipolBinaryConv(val);
+	for(int i =0; i<3; i++)
+		for(int j=(i*4); j < ((i+1)*4); j++)
+			signal[j] = (bbinVal[i]*code[(j-(i*4))]);
+
+	return signal;
+}
+
+/* Begin server set up*/
+int main(int argc, char *argv[])
+{
+	int sockfd, n, snum=0, portno, clilen;
+	char ReadBuffers[3][3];
+	int WriteBuffers[3][16];
+	int newsockfd[3];
+	int codes[3][4]= {{-1,1,-1,1},{-1,-1,1,1},{-1,1,1,-1}};
+	int em[12];
+	struct sockaddr_in serv_addr, cli_addr;
+	if (argc < 2) {
+		fprintf(stderr,"ERROR, no port provided\n");
+		exit(1);
+	}
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+		error("ERROR opening socket");
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	portno = atoi(argv[1]);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+		error("ERROR on binding");
+	clilen = sizeof(cli_addr);
+	
+	while(snum < 3) {
+		listen(sockfd,5);
+		newsockfd[snum] = accept(sockfd, (struct sockaddr *) &cli_addr, (socklen_t *)&clilen);
+		if (newsockfd < 0)
+			error("ERROR: Child process could not be accepted");
+		else
+			snum++;
+	}
+	
+	char tempbuffer[3];
+	for(int i=0; i < 3; i++) {
+		n = read(newsockfd[i],tempbuffer,3); //uses the buffer to read from sender/receiver program
+		printf("Here is the message from child %c: , Value = %c, Destination = %c\n", tempbuffer[0],tempbuffer[2],tempbuffer[1]);
+		for(int j=0; j<3;j++)
+			ReadBuffers[i][j] = tempbuffer[j];
+		bzero(tempbuffer,3);
+	}
+
+	if (n<0)
+		error("ERROR reading from a socket");
+
+	/* Compute sum of all the EM[p] signals */
+	for(int i=0; i<3; i++) {
+		int* emp = Encoder(codes[i], (ReadBuffers[i][2]-'0') );
+		for(int j=0; j < 12; j++)
+			em[j] += emp[j];
+	}
+	
+	/*
+	 Write off EM signal and Walsh Code that corresponds to the destination specified by each child process. (ROUTER BLOCK)
+	 	Write buffer format =  [EM signal (0...11), Walsh Code of Sender (12...15)]
+	 */
+	for(int i=0; i < 3; i++) {
+		for(int j=0; j < 12; j++)
+			WriteBuffers[i][j] = em[j];
+		if((ReadBuffers[i][1]-'0') == 1) // if dest. is 1, get code of sender
+			for(int j= 12; j < 16; j++)
+				WriteBuffers[0][j] = codes[((ReadBuffers[i][0]-'0')-1)][j-12];
+		else if((ReadBuffers[i][1]-'0') == 2) // if dest. is 2, get code of sender
+			for(int j= 12; j < 16; j++)
+				WriteBuffers[1][j] = codes[((ReadBuffers[i][0]-'0')-1)][j-12];
+		else if((ReadBuffers[i][1]-'0') == 3) // if dest. is 3, get code of sender
+			for(int j= 12; j < 16; j++)
+				WriteBuffers[2][j] = codes[((ReadBuffers[i][0]-'0')-1)][j-12];
+	}
+
+	int buff[16];
+	//convert to endian friendly data and write back
+	for(int i = 0; i < 3; i++) {
+		for(int j=0; j < 16; j++)
+			buff[j] = htonl(WriteBuffers[i][j]);
+		n = write(newsockfd[i], buff, sizeof(int)*16);
+		if (n < 0)
+			error("ERROR writing to socket for child process 1");
+		else
+			sleep(1);
+		close(newsockfd[i]);
+	}
+	
+	return 0;
+}
